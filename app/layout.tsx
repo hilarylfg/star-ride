8e0fa9859c49f78aada5d60baac5034a2a71305e
Nikolay Polyakov <misterkola337@gmail.com>
2024-12-30 06:24:20 +0300

    upd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,6 @@
 import type {Metadata} from "next";
 import {ReactNode} from "react";
-import {Comfortaa, Poppins} from "next/font/google";
-
-const comfortaa = Comfortaa({
-    subsets: ['latin'],
-    variable: '--font-comfortaa',
-    weight: ['500', '600', '700'],
-});
-
-const poppins = Poppins({
-    subsets: ['latin'],
-    variable: '--font-poppins',
-    weight: ['500', '600', '700'],
-});
+import "@/shared/styles/main.css"
 
 export const metadata: Metadata = {
     title: "StarRide | Главная",
@@ -21,7 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: ReactNode; }>) {
     return (
         <html lang="en">
-        <body className={`${poppins.variable} ${comfortaa.variable}`}>
+        <body>
         {children}
         </body>
         </html>
--- shared/components/shared/MapComponent/MapComponent.tsx
+++ shared/components/shared/MapComponent/MapComponent.tsx
@@ -1,18 +1,22 @@
 "use client";
 
-import {YMaps, Map, Placemark} from '@pbe/react-yandex-maps';
+import {YMaps, Map} from '@pbe/react-yandex-maps';
 
 export function MapComponent() {
     return (
         <>
             <div className="map">
-                <YMaps query={{apikey: process.env.NEXT_PUBLIC_YANDEX_MAPS_API_KEY}}>
+                <YMaps query={{apikey: process.env.NEXT_PUBLIC_YANDEX_MAPS_API_KEY, }}>
                     <Map
                         defaultState={{center: [55.751574, 37.573856], zoom: 10}}
+                        options={{
+                            copyrightUaVisible: false,
+                            copyrightProvidersVisible: false,
+                            copyrightLogoVisible: false,
+                        }}
                         width="500px"
-                        height="1000px"
+                        height="100vh"
                     >
-                        <Placemark geometry={[55.751574, 37.573856]}/>
                     </Map>
                 </YMaps>
             </div>
